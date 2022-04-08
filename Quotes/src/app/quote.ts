493bc14ced1a02b5b4917c57634b307e6d0c40d1
Maureen-Muriithi <moh2wanja@gmail.com>
2022-04-08 23:24:02 +0300

    Add class(quote) and add quote constructor

---
+++ Quotes/src/app/quote.ts
@@ -0,0 +1,15 @@
+export class Quote {
+    id: number;
+    quote: string;
+    author: string;
+    upvotes: number;
+    downvotes: number;
+    
+    constructor(id:number,quote:string, author:string, upvotes:number, downvotes:number ){
+        this.id = id;
+        this.quote = quote;
+        this.author = author;
+        this.upvotes = upvotes;
+        this.downvotes = downvotes;
+    }
+}
--- Quotes/src/app/quotes/quotes.component.ts
+++ Quotes/src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quotes',
@@ -7,6 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesComponent implements OnInit {
 
+  quotes: Quote[] = [
+    new Quote(1, "Quote1", "by Ghandhi", 0, 0),
+    new Quote(2, "Quote2", "by Ghandhi", 0, 0),
+    new Quote(3, "Quote3", "by Ghandhi", 0, 0),
+    new Quote(4, "Quote4", "by Ghandhi", 0, 0),
+    new Quote(5, "Quote5", "by Ghandhi", 0, 0),
+    new Quote(6, "Quote6", "by Ghandhi", 0, 0)
+
+
+
+
+
+  ]
   constructor() { }
 
   ngOnInit(): void {
