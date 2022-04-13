52f6c9f3bcd37dba8ce7e3f6d00931f61536069b
Maureen-Muriithi <moh2wanja@gmail.com>
2022-04-13 20:55:06 +0300

    Auto-generated commit [ci skip]

--- Quotes/src/app/days-checker.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DaysCheckerPipe } from './days-checker.pipe';
-
-describe('DaysCheckerPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DaysCheckerPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
